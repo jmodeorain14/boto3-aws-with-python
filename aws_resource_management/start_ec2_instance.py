b87517a67be113529e393c67b983679247a4b3a5
Joseph O'Deorain <jmodeorain14@hotmail.com>
2023-12-29 20:50:36 +0100

    chore: update AWS EC2 instance ID

--- aws_resource_management/start_ec2_instance.py
+++ aws_resource_management/start_ec2_instance.py
@@ -1,12 +1,10 @@
 import boto3
 
-# Create a Boto3 EC2 resource object
+# Create a Boto3 EC2 client object
 ec2 = boto3.client("ec2")
 
 # Define the Jtl Reporter EC2 Instance ID
-instance_id = 'i-0d06727157ab91576'
+instance_id = "i-09f8e38adcf744509"
 
 # Start the Jtl Reporter EC2 instance
-response = ec2.start_instances(
-    InstanceIds=[instance_id]
-)
+response = ec2.start_instances(InstanceIds=[instance_id])
